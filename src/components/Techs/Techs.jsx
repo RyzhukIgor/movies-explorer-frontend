62c5d75024ee87c4c6ae4b1331873f1ccefecc59
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-05-01 02:52:01 +0300

    в процессе раздел Techs

---
+++ src/components/Techs/Techs.jsx
@@ -0,0 +1,10 @@
+function Techs() {
+    return (
+        <section className="techs">
+            <h2 className="about-project__title">Технологии</h2>
+            <h3 className="promo__description">7 технологий</h3>
+        </section>
+    );
+}
+
+export default Techs;
