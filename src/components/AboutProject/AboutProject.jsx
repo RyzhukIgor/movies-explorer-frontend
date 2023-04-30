f2015ecd185bb9e858e04691bd9c69c82c453bc8
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-05-01 01:20:42 +0300

    в процессе About project

---
+++ src/components/AboutProject/AboutProject.jsx
@@ -0,0 +1,29 @@
+function AboutProject() {
+    return (
+        <section className="about-project">
+            <h2 className="about-project__title">О проекте</h2>
+            <div className="about-project__description-blocks">
+                <div className="about-project__description-item">
+                    <h3 className="about-project__subtitle">
+                        Дипломный проект включал 5 этапов
+                    </h3>
+                    <p className="about-project__info">
+                        Составление плана, работу над бэкендом, вёрстку,
+                        добавление функциональности и финальные доработки.
+                    </p>
+                </div>
+                <div className="about-project__description-item">
+                    <h3 className="about-project__subtitle">
+                        На выполнение диплома ушло 5 недель
+                    </h3>
+                    <p className="about-project__info">
+                        У каждого этапа был мягкий и жёсткий дедлайн, которые
+                        нужно было соблюдать, чтобы успешно защититься.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default AboutProject;
