2c6ddff037981a0a221f35479f788a123b5081f1
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-05-01 23:55:26 +0300

    подогнал под pixelperfect

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -7,12 +7,12 @@ function AboutMe() {
             <article className="about-me__infoblock">
                 <div className="about-me__info-person">
                     <h3 className="about-me__name">Виталий</h3>
-                    <h4 className="about-project__subtitle">
+                    <h4 className="about-me__subtitle">
                         Фронтенд-разработчик, 30 лет
                     </h4>
-                    <p className="about-project__info">
+                    <p className="about-me__info">
                         Я родился и живу в Саратове, закончил факультет
-                        экономики СГУ. У меня есть жена и дочь. Я люблю слушать
+                        экономики СГУ. У меня есть жена<br/> и дочь. Я люблю слушать
                         музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С
                         2015 года работал в компании «СКБ Контур». После того,
                         как прошёл курс по веб-разработке, начал заниматься
