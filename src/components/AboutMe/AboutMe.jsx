02f2bc357c74f0ec63cdc9b0ac16e182de1162cc
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-05-01 19:52:44 +0300

    блок About me готов

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,29 @@
+import Avatar from '../../images/avatar.jpg';
+
+function AboutMe() {
+    return (
+        <section className="about-me">
+            <h2 className="about-project__title">Студент</h2>
+            <article className="about-me__infoblock">
+                <div className="about-me__info-person">
+                    <h3 className="about-me__name">Виталий</h3>
+                    <h4 className="about-project__subtitle">
+                        Фронтенд-разработчик, 30 лет
+                    </h4>
+                    <p className="about-project__info">
+                        Я родился и живу в Саратове, закончил факультет
+                        экономики СГУ. У меня есть жена и дочь. Я люблю слушать
+                        музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С
+                        2015 года работал в компании «СКБ Контур». После того,
+                        как прошёл курс по веб-разработке, начал заниматься
+                        фриланс-заказами и ушёл с постоянной работы.
+                    </p>
+                    <a className="about-me__link" href="github.com">Github</a>
+                </div>
+                <img src={Avatar} alt="аватар студента" className="about-me__avatar" />
+            </article>
+        </section>
+    );
+}
+
+export default AboutMe;
--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -2,7 +2,20 @@ function Techs() {
     return (
         <section className="techs">
             <h2 className="about-project__title">Технологии</h2>
-            <h3 className="promo__description">7 технологий</h3>
+            <h3 className="techs__description">7 технологий</h3>
+            <p className="techs__subtitle">
+                На курсе веб-разработки мы освоили технологии, которые применили
+                в дипломном проекте.
+            </p>
+            <ul className="techs__stack">
+                <li className="techs__stack-item">HTML</li>
+                <li className="techs__stack-item">CSS</li>
+                <li className="techs__stack-item">JS</li>
+                <li className="techs__stack-item">React</li>
+                <li className="techs__stack-item">Git</li>
+                <li className="techs__stack-item">Express.js</li>
+                <li className="techs__stack-item">mongoDB</li>               
+            </ul>
         </section>
     );
 }
