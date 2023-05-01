63a03a63d8a136c4e389cc9d0bffd85eba7f98dd
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-05-01 22:23:23 +0300

    выполнен блок portfolio компонента main

---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,34 @@
+import icon from "../../images/portfolio-icon.png";
+
+function Portfolio() {
+    return <section className="portfolio">
+        <h2 className="portfolio__title">
+            Портфолио
+        </h2>
+        <nav>
+            <ul className="portfolio__items">
+                <li className="portfolio__item">
+                    <a href="https://github.com/RyzhukIgor/how-to-learn" className="portfolio__link">
+                    <p className="portfolio__description">Статичный сайт</p>
+                    <img src={icon} alt="стрелка-ссылка" className="portfolio__icon" />
+                    </a>
+                </li>
+                <li className="portfolio__item">
+                    <a href="https://github.com/RyzhukIgor/russian-travel" className="portfolio__link">
+                    <p className="portfolio__description">Адаптивный сайт</p>
+                    <img src={icon} alt="стрелка-ссылка" className="portfolio__icon" />
+                    </a>
+                </li>
+                <li className="portfolio__item">
+                    <a href="https://github.com/RyzhukIgor/react-mesto-api-full-gha" className="portfolio__link">
+                    <p className="portfolio__description">Одностраничное приложение</p>
+                    <img src={icon} alt="стрелка-ссылка" className="portfolio__icon" />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+    </section>;
+}
+
+export default Portfolio;
