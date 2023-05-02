7fc8a35aa04ccf4925a07e8fdeb6d6f62f38cddb
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-05-03 02:40:42 +0300

    checkbox done

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,12 +1,25 @@
 function SearchForm() {
-    return(
+    return (
         <section className="search">
             <form className="search__items">
-                <input className="search__input" placeholder="Фильм" name="films" />
-                <button type="button" className="search_button" ></button>
+                <input
+                    className="search__input"
+                    placeholder="Фильм"
+                    name="films"
+                />
+                <button type="button" className="search_button">
+                    Поиск
+                </button>
             </form>
+            <div className="search__checkbox-container">
+            <label className="search__checkbox" htmlFor="checkbox">
+                <input type="checkbox" className="search__input-checkbox" id="checkbox" />
+                <div className="search__slider"></div>
+            </label>
+            <p className="search__category">Короткометражки</p>
+            </div>
         </section>
-    )
+    );
 }
 
 export default SearchForm;
