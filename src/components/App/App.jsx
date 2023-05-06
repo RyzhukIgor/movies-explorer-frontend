1777d4979f710a8834e02457c7193c56d69c4db9
Igor Ryzhuk <ig.ryzhuk@yandex.ru>
2023-05-07 00:57:59 +0300

    404 done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,7 @@ import SavedMovies from "../SavedMovies/SavedMovies";
 import Profile from "../Profile/Profile";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
+import NotFoundPage from "../NotFoundPage/NotFoundPage";
 
 function App() {
     return (
@@ -54,6 +55,7 @@ function App() {
                     />
                     <Route path="/signup" element={<Register />} />
                     <Route path="/signin" element={<Login />} />
+                    <Route path="*" element={<NotFoundPage />} />
                 </Routes>
                 
             </div>
---
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+function NotFoundPage() {
+    return (
+        <main className="not-found">
+            <h1 className="not-found__title">404</h1>
+            <p className="not-found__subtitle">Страница не найдена</p>
+            <NavLink className="not-found__link" to="/">Назад</NavLink>
+        </main>
+    );
+}
+
+export default NotFoundPage;
